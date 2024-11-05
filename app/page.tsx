36df3ae9fb14187ab245dcb9ceda8aebf46206cf
Samuel Berrus <samuelberrus@gmail.com>
2024-11-05 18:38:35 +0100

    Añade elementos basicos a la estructura de la we

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from './components/Header'
+import HeroSection from './sections/HeroSection'
+import ServicesSection from './sections/ServicesSection'
+import CTASection from './sections/CTASection'
+import FooterSection from './sections/FooterSection'
+import TeamSection from './sections/TeamSection'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      {/* header */}
+      <Header />
+      {/* hero section */}
+      <HeroSection />
+      {/* services section */}
+      <ServicesSection />
+      {/* team section */}
+      <TeamSection />
+      {/* CTA */}
+      <CTASection />
+      {/* Footer */}
+      <FooterSection />
+    </>
   )
 }
 
